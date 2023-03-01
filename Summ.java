5d70ebf05476da82ee3c1404c01193690774b2e0
LikithaSriB <126647286+LikithaSriB@users.noreply.github.com>
2023-03-01 11:46:34 +0530

    create sum

---
+++ Summ.java
@@ -0,0 +1,10 @@
+public class Summ{
+public static void main(String args[]){
+double s=0;
+for(String x:args){
+
+s=s+Double.parseDouble(x);
+}
+System.out.println(s);
+}
+}
---
+++ sum.java
@@ -0,0 +1,9 @@
+class Sum{
+public static void main(String args[]){
+double s=0;
+for(String x:args){
+
+s=s+Double.parseDouble(x);
+}
+System.out.println(s);
+}}
